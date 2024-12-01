cea89304a24b53301961af4d902b996a23365074
Solly <post2solly@gmail.com>
2024-12-01 15:29:38 +1000

    navbar added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-
+import { NavbarComponent } from './navbar/navbar.component';
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    MatSlideToggleModule],
+    NavbarComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
