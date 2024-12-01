d0728f35d0c1fc6a4567f94540bf70650aaa94fc
Solly <post2solly@gmail.com>
2024-12-01 16:27:34 +1000

    configuration service added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ConfigurationService } from '../services/configuration.service';
+
 @Component({
   selector: 'app-root',
   imports: [
@@ -10,6 +12,16 @@ import { NavbarComponent } from './navbar/navbar.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'URL.Shortener.App';
+export class AppComponent implements OnInit {
+  title: string;
+  appVersion: string;
+
+  constructor(private configService: ConfigurationService) {
+    this.title = this.configService.appTitle;
+    this.appVersion = this.configService.appVersion;
+  }
+
+  ngOnInit(): void {
+    console.log(this.configService.restUrl);
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,13 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { ConfigurationService } from '../services/configuration.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes), provideAnimationsAsync()
+    provideRouter(routes), provideAnimationsAsync(),
+    ConfigurationService
   ]
 };
