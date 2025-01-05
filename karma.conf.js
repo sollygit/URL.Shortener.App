d208bcc008c2b7c69d7390aa736e538072fd546b
Solly <post2solly@gmail.com>
2025-01-05 17:31:34 +1000

    app renamed

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/spa_angular_typescript_hello-world'),
+      dir: require('path').join(__dirname, './coverage/url-shortener-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
